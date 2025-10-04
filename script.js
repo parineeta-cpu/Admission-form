50dc56fe2f1aa7789377c773cedbbd8f0fa8edd1
parineeta-cpu <cpari4097@gmail.com>
2025-10-04 11:29:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -17,13 +17,11 @@ form.addEventListener("submit",(e)=>{
    const emailInput=document.querySelector(".email");
    const studentImg=document.querySelector(".studentImg");
    const checkbox=document.querySelector(".checkbox");
-   const qualification=document.querySelector(".edu-qualification");
-   const board=document.querySelector(".edu-board");
-   const passingYear=document.querySelector(".edu-year");
    const result=document.querySelector(".edu-marks");
 
    if(nameInput.value.length < 3){
-    alert("Name must be atleat 3 characters long");
+    const error1 = document.querySelector(".error1");
+    error1.innerText="Name must be atleast 3 characters long.";
     return;
    }
   
@@ -36,65 +34,57 @@ form.addEventListener("submit",(e)=>{
       age--;
     }
     if(age<10||age>80){
-      alert("Age should be between 10-80");
+      const error2 = document.querySelector(".error2");
+    error2.innerText="Age should be between 10-80.";
       return;
     }
    
-    if(genderInput.value===""){
-    alert("Please select your gender");
-     return;
-      
-   }
+
    if(adhaarNoInput.value && !/^\d{12}$/.test(adhaarNoInput.value)){
-    alert("please enter a valid Adhaar No");
+    const error3 = document.querySelector(".error3");
+    error3.innerText="Please enter a valid Adhaar No.";
+    
     return;
    }
    if(GuardianNameInput.value.length < 3){
-    alert("Guardian name must be atleat 3 characters long");
+    const error4 = document.querySelector(".error4");
+    error4.innerText="Guardian name must be atleast 3 characters long.";
+    
     return;
    }
-   if(occupationInput.value===""){
-    alert("Please enter your Occupation");
-    return;
-   }
-   if(addressInput.value===""){
-    alert("Please enter your Permanent Address");
-    return;
-   }
-   if(districtInput.value===""){
-    alert("Please enter your District");
-    return;
-   }
-   if(stateInput.value===""){
-    alert("Please enter your State");
-    return;
-   }
+   
    if(pincodeInput.value.length!==6){
-    alert("Please enter a valid pincode");
+    const error5 = document.querySelector(".error5");
+    error5.innerText="Please enter a valid pincode.";
+   
     return;
    }
    if(numberInput.value.length!==10){
-    alert("Please enter a valid 10-digit number.")
+     const error6 = document.querySelector(".error6");
+    error6.innerText="Please enter a valid 10-digit number.";
+    
      return;
     
    }
    if(number2Input.value.length!==10){
-    alert("Please enter a valid 10-digit number.");
+     const error7 = document.querySelector(".error7");
+    error7.innerText="Please enter a valid 10-digit number.";
+   
 return;
   }else{
     if(number2Input.value===numberInput.value){
-alert("Mobile No and Emergency No could't be same");
+alert("Mobile No and Emergency No could't be same.");
 return;
 }
   }   
    
    if(!emailInput.checkValidity()){
-    alert("Please enter a valid email");
+    alert("Please enter a valid email.");
      return;
    }
 
    if(!studentImg.files[0]){
-    alert("Please upload an image");
+    alert("Please upload an image.");
     return;
    }else{
    const img = new Image();
@@ -104,22 +94,28 @@ const width = img.width;
 const height = img.height;
 
    if(width<200 || height<200 || width>2000 || height>2000){
-    alert("Image Dimensions must be between 200*200 and 2000*2000");
+     const error8 = document.querySelector(".error8");
+    error8.innerText="Image Dimensions must be between 200*200 and 2000*2000.";
+    
     return;
    }
    else{
     if(studentImg.files[0].size > 200 * 1024){
-      alert("Image size must be less than 200 KB");
+       const error8 = document.querySelector(".error8");
+    error8.innerText="Image size must be less than 200 KB.";
+   
       return;
     }
   
    }
    if(!checkbox.checked){
-    alert("Please agree to all Terms&conditions");
+     const error9 = document.querySelector(".error9");
+    error9.innerText="Please agree to all Terms&conditions.";
+    
      return;
    }
    if(result.value<0 || result.value>100){
-alert("Please enter your valid marks between 1-100");
+alert("Please enter your valid marks between 1-100.");
 return;
    }
    else {
